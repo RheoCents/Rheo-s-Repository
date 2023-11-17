6687d7d972df5d9a0ba5a9582d7ec257b4a627a5
Rheo <centinaraphael0157@gmail.com>
2023-11-17 20:10:49 +0800

    First SeatWork

---
+++ Seatwork #1.py
@@ -0,0 +1,6 @@
+print("Welcome to the 1st seatwork of PLD")
+a = input("What is your name? ")
+b = input("And what is your dream job/profession ")
+
+print("Hi, my name is " + str(a))
+print("And I want to be a " + str(b))
